04ee9d564152ff5176ce1f34816913a84d144715
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-04-23 20:25:56 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  globals: {
+    require: false,
+    module: true,
+    _: true,
+    describe: false,
+    expect: false,
+    jest: false,
+    it: false
+  },
+  "extends": ["eslint:recommended"],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "plugins": [
+  ],
+  "rules": {
+    "curly": ["error", "multi"],
+    "complexity": ["error", 4],
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "max-depth": ["error", 2],
+    "max-len": ["error", 120],
+    "max-params": ["error", 3],
+    "max-statements": ["error", 6],
+    "no-else-return": ["error"],
+    "no-extra-parens": ["error"],
+    "no-shadow": ["error"],
+    "no-unneeded-ternary": ["error"],
+    "quotes": ["error", "single"],
+    "semi": ["error", "never"],
+    "no-unused-vars": "off"
+  }
+};
--- index.js
+++ index.js
@@ -1,1 +1,99 @@
-document.getElementById('root').insertAdjacentHTML('beforeend', '<h1>Hello ES2015!</h1>');
+var _ = require('lodash')
+require('./style.css')
+
+function getResult(doc) {
+  return _.fromPairs(_.map(doc.querySelectorAll('ClassResult'), pair))
+
+  function pair(classResult) {
+    return [
+      classResult.querySelector('Class Name').textContent,
+      _.map(classResult.querySelectorAll('PersonResult'), getPerson)
+    ]
+  }
+
+  function getPerson(person) {
+    var time = person.querySelector('Result > Time')
+    var position = person.querySelector('Result > Position')
+    return {
+      name: _.map(['Given', 'Family'], field => person.querySelector(`Person > Name > ${field}`).textContent).join(' '),
+      position: position && position.textContent,
+      time: getTime(time && time.textContent, person.querySelector('Result > Status').textContent),
+      splits: getSplits(person.querySelectorAll('Result > SplitTime > Time'))
+    }
+
+    function getSplits(splits) {
+      var prevSeconds = 0
+      return _.map(_.map(splits, 'textContent'), lapTime)
+
+      function lapTime(splitSeconds) {
+        var lapSeconds = splitSeconds - prevSeconds
+        prevSeconds = splitSeconds
+        return getTime(lapSeconds)
+      }
+    }
+
+    function getTime(totalSeconds, status = 'OK') {
+      var statuses = {
+        MissingPunch: 'felst.',
+        DidNotStart: 'ej start'
+      }
+
+      if (status === 'OK')
+        return getMmSs()
+
+      return statuses[status] || status
+
+      function getMmSs() {
+        var seconds = totalSeconds % 60
+        var minutes = (totalSeconds - seconds) / 60
+        var pad = seconds < 10 ? '0' : ''
+        return `${minutes}:${pad}${seconds}`
+      }
+    }
+  }
+}
+
+function ajax(callback) {
+  var request = new XMLHttpRequest()
+  request.open('GET', 'result.xml', true)
+
+  request.onload = function () {
+    if (this.status >= 200 && this.status < 400)
+      callback(this.responseXML)
+  }
+
+  request.send()
+}
+
+ajax(responseXml => {
+  var root = document.getElementById('root')
+
+  _.map(getResult(responseXml), (persons, className) => {
+    root.insertAdjacentHTML('beforeend',
+      `<table>
+         <caption>${className}</caption>
+         <tbody>${getPersons(persons)}</tbody>
+       </table>`)
+  })
+
+  function getPersons(persons) {
+    return _.map(persons, getPerson).join('')
+  }
+
+  function getPerson(p) {
+    return `<tr>
+              <td>${p.position || ''}</td>
+              <td class="name">${p.name}</td>
+              ${getSplits(p.splits)}
+              <td class="total">${p.time}</td>
+            </tr>`
+  }
+
+  function getSplits(splits) {
+    return _.map(splits, getSplit).join('')
+  }
+
+  function getSplit(split) {
+    return `<td class="split">${split}</td>`
+  }
+})
