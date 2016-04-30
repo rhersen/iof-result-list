eafeeb92886b3d9c8424556e9acb6e9b177103bf
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-04-30 13:46:59 +0200

    Separate DOM extraction from calculations

--- calculate.js
+++ calculate.js
@@ -1,7 +1,41 @@
-module.exports = function (classResult) {
+var map = require('lodash.map')
+
+function calculate(raw) {
   return {
-    name: classResult.Name,
-    persons: classResult.PersonResults
+    name: raw.Name,
+    persons: map(raw.PersonResults, getPerson)
   }
 }
 
+function getPerson(raw) {
+  return {
+    name: raw.Given,
+    position: raw.Position,
+    time: getTime(raw.Time, raw.Status),
+    splits: map(raw.splits, getMmSs)
+  }
+}
+
+function getTime(totalSeconds, status = 'OK') {
+  var statuses = {
+    MissingPunch: 'felst',
+    DidNotStart: 'dns'
+  }
+
+  if (status === 'OK')
+    return getMmSs(totalSeconds)
+
+  return statuses[status] || status
+}
+
+function getMmSs(totalSeconds) {
+  var seconds = totalSeconds % 60
+  var minutes = (totalSeconds - seconds) / 60
+  return `${pad(minutes, '&#8199;')}:${pad(seconds, '0')}`
+
+  function pad(n, c) {
+    return n < 10 ? c + n : n
+  }
+}
+
+module.exports = calculate
--- getClass.js
+++ getClass.js
@@ -2,46 +2,18 @@ var map = require('lodash.map')
 
 module.exports = function ($classResult) {
   return {
-    Name: getTextContent($classResult, 'Class Name'),
+    Name: getTextContent($classResult, 'Class > Name'),
     PersonResults: map($classResult.querySelectorAll('PersonResult'), getPerson)
   }
 
   function getPerson($person) {
     return {
-      name: map(['Given', 'Family'], getNameField).join(' '),
-      position: getTextContent($person, 'Result > Position'),
-      time: getTime(getTextContent($person, 'Result > Time'), getTextContent($person, 'Result > Status')),
-      splits: getSplits($person.querySelectorAll('Result > SplitTime > Time'))
-    }
-
-    function getNameField(field) {
-      return getTextContent($person, `Person > Name > ${field}`)
-    }
-
-    function getSplits(splits) {
-      return map(map(splits, 'textContent'), getMmSs)
-    }
-
-    function getTime(totalSeconds, status = 'OK') {
-      var statuses = {
-        MissingPunch: 'felst',
-        DidNotStart: 'dns'
-      }
-
-      if (status === 'OK')
-        return getMmSs(totalSeconds)
-
-      return statuses[status] || status
-    }
-
-    function getMmSs(totalSeconds) {
-      var seconds = totalSeconds % 60
-      var minutes = (totalSeconds - seconds) / 60
-      return `${pad(minutes, '&#8199;')}:${pad(seconds, '0')}`
-
-      function pad(n, c) {
-        return n < 10 ? c + n : n
-      }
+      Given: getTextContent($person, 'Person > Name > Given'),
+      Family: getTextContent($person, 'Person > Name > Family'),
+      Position: getTextContent($person, 'Result > Position'),
+      Time: getTextContent($person, 'Result > Time'),
+      Status: getTextContent($person, 'Result > Status'),
+      splits: map($person.querySelectorAll('Result > SplitTime > Time'), 'textContent')
     }
   }
 
