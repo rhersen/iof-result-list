4c80da4dcb1c4dbf847d86da501ec6d4e02469a6
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-04-30 14:35:07 +0200

    Decrease max-len and max-statements

--- insertHtml.js
+++ insertHtml.js
@@ -12,36 +12,35 @@ module.exports = responseXml => {
   forEach(responseXml.querySelectorAll('ClassResult'), classDom => {
     root.insertAdjacentHTML('beforeend', classHtml(calculate(getClass(classDom))))
   })
+}
+function getEventName(doc) {
+  var $name = doc.querySelector('Event > Name')
+  return $name && $name.textContent
+}
 
-  function getEventName(doc) {
-    var $name = doc.querySelector('Event > Name')
-    return $name && $name.textContent
-  }
-
-  function classHtml(classResult) {
-    return `<table>
+function classHtml(classResult) {
+  return `<table>
               <caption>${classResult.name}</caption>
               <tbody>${getPersons(classResult.persons)}</tbody>
             </table>`
-  }
+}
 
-  function getPersons(persons) {
-    return map(persons, getPerson).join('')
+function getPersons(persons) {
+  return map(persons, getPerson).join('')
 
-    function getPerson(p) {
-      return `<tr>
+  function getPerson(p) {
+    return `<tr>
                 <td>${p.position || ''}</td>
                 <td class="name">${p.name}</td>
                 ${getSplits(p.splits)}
                 <td class="total">${p.time}</td>
               </tr>`
 
-      function getSplits(splits) {
-        return map(splits, getSplit).join('')
+    function getSplits(splits) {
+      return map(splits, getSplit).join('')
 
-        function getSplit(split) {
-          return `<td class="split">${split}</td>`
-        }
+      function getSplit(split) {
+        return `<td class="split">${split}</td>`
       }
     }
   }
