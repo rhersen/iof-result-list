24d7a80b010a09588d0331780c26ec2cf87b8ed5
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-05-02 17:46:52 +0200

    Remove persons with status DidNotStart

--- getClass.js
+++ getClass.js
@@ -1,9 +1,11 @@
 var map = require('lodash.map')
 
 module.exports = function ($classResult) {
+  var personResults = map($classResult.querySelectorAll('PersonResult'), getPerson)
+
   return {
     Name: getTextContent($classResult, 'Class > Name'),
-    PersonResults: map($classResult.querySelectorAll('PersonResult'), getPerson)
+    PersonResults: personResults.filter(p => p.Status !== 'DidNotStart')
   }
 
   function getPerson($person) {
