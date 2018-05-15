2d85413fbc36e3c19c7d1c8ddc2bb0a41773b1e4
Rudolf Hersén <rudolf.hersen@schibsted.se>
2018-05-15 15:47:51 +0200

    Prettier

--- calculate.js
+++ calculate.js
@@ -10,8 +10,8 @@ function calculate(raw) {
   const [first] = persons
 
   if (first) {
-    var okPersons = filter(persons, 'ok')
-    var best = map(first.splits, getBest)
+    okPersons = filter(persons, 'ok')
+    best = map(first.splits, getBest)
 
     forEach(okPersons, p => forEach(p.splits, setBest))
     forEach(okPersons, setMistakes)
@@ -19,7 +19,7 @@ function calculate(raw) {
 
   return {
     name: raw.Name,
-    persons: persons
+    persons: persons,
   }
 
   function getBest(lap, i) {
@@ -27,8 +27,7 @@ function calculate(raw) {
   }
 
   function setBest(lap, i) {
-    if (lap.time === best[i])
-      lap.best = true
+    if (lap.time === best[i]) lap.best = true
 
     lap.ratio = lap.time / best[i]
   }
@@ -40,25 +39,25 @@ function getPerson(raw) {
     position: raw.Position,
     time: getTime(raw.Time, raw.Status),
     ok: raw.Status === 'OK' || !raw.Status,
-    splits: raw.splits && getLaps(raw.Time ? raw.splits.concat(raw.Time) : raw.splits)
+    splits:
+      raw.splits &&
+      getLaps(raw.Time ? raw.splits.concat(raw.Time) : raw.splits),
   }
 
   function getTime(totalSeconds, status = 'OK') {
-    if (status === 'OK' && totalSeconds)
-      return getMmSs(totalSeconds)
+    if (status === 'OK' && totalSeconds) return getMmSs(totalSeconds)
 
-    if (status === 'MissingPunch')
-      return 'felst'
+    if (status === 'MissingPunch') return 'felst'
 
     return status
   }
 
   function getLaps(splits) {
-    var prev = 0
+    let prev = 0
     return map(map(splits, getDiff), getLap)
 
     function getDiff(current) {
-      var diff = current - prev
+      const diff = current - prev
       prev = current
       return diff
     }
@@ -67,13 +66,13 @@ function getPerson(raw) {
   function getLap(seconds) {
     return {
       time: seconds,
-      mmSs: getMmSs(seconds)
+      mmSs: getMmSs(seconds),
     }
   }
 
   function getMmSs(totalSeconds) {
-    var seconds = totalSeconds % 60
-    var minutes = (totalSeconds - seconds) / 60
+    const seconds = totalSeconds % 60
+    const minutes = (totalSeconds - seconds) / 60
 
     return `${minutes}:${pad(seconds, '0')}`
   }
@@ -84,21 +83,19 @@ function getPerson(raw) {
 }
 
 function setMistakes(person) {
-  var ratios = map(person.splits, 'ratio')
+  const ratios = map(person.splits, 'ratio')
   person.median = getMedian(ratios)
 
   forEach(person.splits, split => {
-    if (split.ratio / person.median > 1.2)
-      split.mistake = true
+    if (split.ratio / person.median > 1.2) split.mistake = true
   })
 
   function getMedian(a) {
-    var l = a.length
+    const l = a.length
 
     a.sort()
 
-    if (l % 2)
-      return a[(l - 1) / 2]
+    if (l % 2) return a[(l - 1) / 2]
 
     return (a[l / 2 - 1] + a[l / 2]) / 2
   }
